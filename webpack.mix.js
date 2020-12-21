22536996effae471ecab325277643df7a14917b0
mohamed-saud <mo7amed.saud2020@gmail.com>
2020-12-21 19:01:49 +0200

    delete pages folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,21 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-mix.pug = require('laravel-mix-pug');
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+mix.pug = require("laravel-mix-pug");
 
-mix.setPublicPath('public');
-mix.setResourceRoot('../');
-mix.copyDirectory('resources/images', 'public/images');
+mix.setPublicPath("public");
+mix.setResourceRoot("../");
+mix.copyDirectory("resources/images", "public/images");
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        // processCssUrls: false,
-        postCss: [ tailwindcss('tailwind.js') ],
-    })
-    .pug('pug/*.pug', '../pages/',{
-        pug:{
-            pretty:true,
-            // debug:true
-        }
-    });
+mix
+  .js("resources/js/app.js", "public/js")
+  .sass("resources/sass/app.scss", "public/css")
+  .options({
+    // processCssUrls: false,
+    postCss: [tailwindcss("tailwind.js")],
+  })
+  .pug("pug/*.pug", "../", {
+    pug: {
+      pretty: true,
+      // debug:true
+    },
+  });
